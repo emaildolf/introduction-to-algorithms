b07e1a594a7da0464653f43573ce79cc4f3c4ab8
Luiz Fernando Rodrigues <emaildolf@gmail.com>
2016-09-13 14:46:43 -0300

    Subset Implemented

---
+++ src/Subset.java
@@ -0,0 +1,21 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+public class Subset {
+
+    public static void main(String... args) {
+
+        int K = Integer.parseInt(args[0]);
+
+        RandomizedQueue<String> queue = new RandomizedQueue<>();
+
+        while (!StdIn.isEmpty()) {
+            queue.enqueue(StdIn.readString());
+        }
+
+        for (int k = 0; k < K; k++) {
+            System.out.println(queue.dequeue());
+        }
+    }
+
+}
