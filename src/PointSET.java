34b894f97824b1df956f29d2d967a68d42e2640f
Luiz Fernando Rodrigues <emaildolf@gmail.com>
2016-10-07 17:59:36 -0300

    including test cases for PointSET

--- src/PointSET.java
+++ src/PointSET.java
@@ -84,8 +84,58 @@ public class PointSET {
     }
 
     // unit testing of the methods (optional)
+    // unit testing of the methods (optional)
     public static void main(String[] args) {
 
+        PointSET tree = new PointSET();
+
+        //test insert and contains
+        Point2D p1 = new Point2D(0.7, 0.2);
+        Point2D p2 = new Point2D(0.5, 0.4);
+        Point2D p3 = new Point2D(0.2, 0.3);
+        Point2D p4 = new Point2D(0.4, 0.7);
+        Point2D p5 = new Point2D(0.9, 0.6);
+
+        tree.insert(p1);
+        tree.insert(p2);
+        tree.insert(p3);
+        tree.insert(p4);
+        tree.insert(p5);
+        tree.insert(p5);
+
+        assert tree.size() == 5;
+        assert tree.contains(p1);
+        assert tree.contains(p2);
+        assert tree.contains(p3);
+        assert tree.contains(p4);
+        assert tree.contains(p5);
+        assert !tree.contains(new Point2D(0.5, 0.5));
+
+        //test range
+        RectHV plane = new RectHV(0, 0, 1, 1);
+        List<Point2D> points = (List<Point2D>) tree.range(plane);
+        assert points.size() == 5;
+        assert points.contains(p1);
+        assert points.contains(p2);
+        assert points.contains(p3);
+        assert points.contains(p4);
+        assert points.contains(p5);
+
+        RectHV subPlane = new RectHV(0, 0, 0.3, 0.5);
+        points = (List<Point2D>) tree.range(subPlane);
+        assert points.size() == 1;
+        assert points.contains(p3);
+
+        subPlane = new RectHV(0.3, 0.2, 0.6, 0.8);
+        points = (List<Point2D>) tree.range(subPlane);
+        assert points.size() == 2;
+        assert points.contains(p2);
+        assert points.contains(p4);
+
+        //test nearest
+        Point2D query = new Point2D(0, 0);
+        Point2D nearest = tree.nearest(query);
+        assert p3.equals(nearest);
     }
 
 }
